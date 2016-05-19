91eecfb0a2b5696736a140294a1de4e165c6bfae
pothurazure <pothur.azure@gmail.com>
2016-05-19 05:59:02 -0400

    changed main.ts path

--- systemjs.config.js
+++ systemjs.config.js
@@ -20,7 +20,7 @@
 
     //packages tells the System loader how to load when no filename and/or no extension
     var packages = {
-        'app': { main: 'main.ts', defaultExtension: 'ts' },
+        'app': { main: './app/main.ts', defaultExtension: 'ts' },
         'rxjs': { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { defaultExtension: 'js' },
     };
